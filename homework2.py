5cce6d7aab55644b1c1aa5e6b83b134c94a1e9d8
DaniylMlotek <91188195+DaniylMlotek@users.noreply.github.com>
2021-10-08 17:47:24 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,25 @@
+# Task1
+string = input('enter first word:')
+string1 = input('enter second word:')
+print(string+','+string1)
+
+# Task2.
+a = int(input('enter a digital a:'))
+b = int(input('enter a digital b:'))
+x = int(input('enter a digital x:'))
+
+if a<x<b:
+    print(True)
+else:
+    print(False)
+
+
+#Dob zavdannya:
+
+kolo = int(input('enter radius kola:'))
+
+#S = Pr*2 (formula)
+
+S = 3.14 * kolo**2
+
+print(S)
